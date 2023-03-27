4b5939058635d1342c359518ee13c984e9bc0edd
J Carlos Mamani <jhomanidev@gmail.com>
2023-03-26 23:59:35 -0300

    change ui

--- src/login.php
+++ src/login.php
@@ -40,24 +40,47 @@ if(isset($_POST['user']) && !empty($_POST['user'])) {
   }
 }
 
-$formla = <<<XML
-  <h1>Process ID: $processId</h1>
+?>
+
+<head>
   <link rel="stylesheet" href="/src/assets/login_style.css">
-
-  <form
-    method="POST"
-    name="some-data"
-  >
-    <div class="form-element">
-      <label>Username</label>
-      <input type="text" name="user" pattern="[a-zA-Z0-9]+" required />
-    </div>
-    <div class="form-element">
-      <label>Password</label>
-      <input type="password" name="password" required />
-    </div>
-    <button type="submit">Log In</button>
-  </form>
-XML;
-
-echo $formla;
+</head>
+<body>
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" method="POST">
+<?php
+  echo "<h4>Process id: $processId</h4>"
+?>
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input type="text" name="user" class="login__input" placeholder="User name / Email">
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<input type="password" name="password" class="login__input" placeholder="Password">
+				</div>
+				<button class="button login__submit" type="submit">
+					<span class="button__text">Log In Now</span>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+			<div class="social-login">
+				<h3>log in via</h3>
+				<div class="social-icons">
+					<a href="#" class="social-login__icon fab fa-instagram"></a>
+					<a href="#" class="social-login__icon fab fa-facebook"></a>
+					<a href="#" class="social-login__icon fab fa-twitter"></a>
+				</div>
+			</div>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
+</body>
